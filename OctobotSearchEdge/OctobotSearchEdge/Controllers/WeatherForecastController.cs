2965cb02d1300b78ca29130e944b41ad4bbf9255
Rip Britton <ripbritton3@gmail.com>
2023-11-07 10:54:39 -0600

    using net 7.0

--- OctobotSearchEdge/OctobotSearchEdge/Controllers/WeatherForecastController.cs
+++ OctobotSearchEdge/OctobotSearchEdge/Controllers/WeatherForecastController.cs
@@ -4,30 +4,21 @@ namespace OctobotSearchEdge.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class WeatherForecastController : ControllerBase
+	public class SearchController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
-		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
 
-		private readonly ILogger<WeatherForecastController> _logger;
+		private readonly ILogger<SearchController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		public SearchController(ILogger<SearchController> logger)
 		{
 			_logger = logger;
 		}
 
-		[HttpGet(Name = "GetWeatherForecast")]
-		public IEnumerable<WeatherForecast> Get()
-		{
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = Random.Shared.Next(-20, 55),
-				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-			})
-			.ToArray();
-		}
+		[HttpGet]
+        public IActionResult Get(string query)
+        {
+            // Your search logic here
+            return Ok("You searched for: " + query);
+        }
 	}
 }
