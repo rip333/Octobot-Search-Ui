ec44edd6d65b19510614d51e52b3dc7bf83f1eae
Rip Britton <ripbritton3@gmail.com>
2023-11-09 14:33:05 -0600

    adding the handsome new logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // App.tsx
-import './App.css';
+import styles from "./App.module.css";
 import React, { useState } from 'react';
 import SearchBar from './components/search-bar/SearchBar';
 import Results from './components/results/Results';
 import { Card } from './interfaces/Card';
 import Footer from './components/footer/Footer';
 import FilterOptions from './components/filters/FilterOptions';
+import logo from './logo.png';
 
 function App() {
   const [searchResults, setSearchResults] = useState<Card[]>([]);
@@ -25,9 +26,11 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="#" onClick={resetToHomePage} style={{ textDecoration: 'none', color: 'inherit' }}>OCTOBOT</a>
+    <div className={styles.Octobot}>
+      <header className={styles.OctobotHeader}>
+        <a href="#" onClick={resetToHomePage} className={styles.OctobotLabel}>
+          <img src={logo} className={styles.OctobotLogo} alt="logo" />
+        </a>
         <SearchBar onSearch={handleSearchResults} incomplete={incomplete} origin={origin} />
         <FilterOptions
           incomplete={incomplete}
